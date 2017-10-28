fed2e5d26318a07c073173dbae2c898c65a75162
Dirk Dresch <dresch@unknown.ch>
2017-10-28 20:49:47 +0200

    Fix main html template

--- project/client/main.js
+++ project/client/main.js
@@ -1,28 +1,15 @@
 import { Template } from 'meteor/templating'
-import { ReactiveVar } from 'meteor/reactive-var'
 import { History } from '../imports/collections/history.js'
 import './main.html'
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.messages.onCreated(function messagesOnCreated() {
   Meteor.subscribe('history', () => {
     console.log('History subscription ready.')
   })
 })
 
-Template.hello.helpers({
-  counter: function() {
-    return Template.instance().counter.get();
-  },
+Template.messages.helpers({
   messages: function() {
     return History.find({})
   }
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+})
